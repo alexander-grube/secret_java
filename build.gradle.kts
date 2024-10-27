93d57f4ed842bd5a685fb59ab58c123093cd2018
Alexander Grube <alexp.grube@googlemail.com>
2024-10-27 16:24:49 +0100

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,12 +15,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.undertow:undertow-core:2.3.15.Final")
-    implementation("io.undertow:undertow-servlet:2.3.15.Final")
-    implementation("io.undertow:undertow-websockets-jsr:2.3.15.Final")
-    implementation("com.alibaba.fastjson2:fastjson2:2.0.52")
-    implementation("org.postgresql:postgresql:42.7.3")
-    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("io.undertow:undertow-core:2.3.18.Final")
+    implementation("io.undertow:undertow-servlet:2.3.18.Final")
+    implementation("io.undertow:undertow-websockets-jsr:2.3.18.Final")
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.53")
+    implementation("org.postgresql:postgresql:42.7.4")
+    implementation("com.zaxxer:HikariCP:6.0.0")
     implementation("io.github.cdimascio:dotenv-java:3.0.1")
     testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M1")
